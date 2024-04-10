209ce1625db7a8c18968b4f2a01cae556b165092
João Paulo Rios <joao.paulo.rios@hotmail.com>
2024-04-09 23:11:01 -0300

    fix: open box logic

--- src/components/NewStepper.tsx
+++ src/components/NewStepper.tsx
@@ -1,46 +1,40 @@
-import * as React from 'react';
-import Image from 'next/image';
-import React, { useContext } from 'react';
+import Image from "next/image";
+import React, { useContext } from "react";
 
-// MUI imports 
-import Box from '@mui/material/Box';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepButton from '@mui/material/StepButton';
-import Typography from '@mui/material/Typography';
-import { Button } from '@mui/material';
+// MUI imports
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepButton from "@mui/material/StepButton";
+import Typography from "@mui/material/Typography";
+import { Button } from "@mui/material";
 
-//my imports 
-import boxclosed from '../../public/boxclosed.png'
-import boxopened from '../../public/boxopened.png'
-import { boxOpenedContext } from '@/context/boxopenedcontext';
+//my imports
+import boxclosed from "../../public/boxclosed.png";
+import boxopened from "../../public/boxopened.png";
 
 const steps = [
   {
-    text: 'Quem somos?',
-    value: 0
+    text: "Quem somos?",
+    value: 0,
   },
   {
-    text: 'Por que a Torabox?',
-    value: 1
+    text: "Por que a Torabox?",
+    value: 1,
   },
   {
-    text: 'Processo comercial',
-    value: 2
-  }]
+    text: "Processo comercial",
+    value: 2,
+  },
+];
 
 export default function NewStepper() {
-
-  // //useContext
-  // const { boxOpened, setBoxOpened } = useContext(boxOpenedContext);
-
-
   const [activeStep, setActiveStep] = React.useState(0);
   const [completed, setCompleted] = React.useState<{
     [k: number]: boolean;
   }>({});
 
-  const [box, setBox] = React.useState(false)
+  const [box, setBox] = React.useState(false);
 
   const totalSteps = () => {
     return steps.length;
@@ -50,58 +44,52 @@ export default function NewStepper() {
     setActiveStep(step);
   };
 
-
   return (
-    <Box sx={{ width: '100%' }}>
+    <Box sx={{ width: "100%" }}>
       <Stepper nonLinear activeStep={activeStep}>
         {steps.map((step, index) => (
           <Step key={step.text} completed={completed[index]}>
             <StepButton color="inherit" onClick={handleStep(index)}>
-
-
               <Button>
-                <Image
-                  className='w-32'
-                  onClick={() => setBox(prevMode => !prevMode)}
-                  src={activeStep === step.value ? boxclosed : boxopened}
-                  // src={boxclosed}
-                  alt="box-closed"
-                  priority={false}
-                />
+                {activeStep == step.value ? (
+                  <Image
+                    className="w-32"
+                    onClick={() => setBox((prevMode) => !prevMode)}
+                    src={boxopened}
+                    alt="box-closed"
+                    priority={false}
+                  />
+                ) : (
+                  <Image
+                    className="w-32"
+                    onClick={() => setBox((prevMode) => !prevMode)}
+                    src={boxclosed}
+                    alt="box-closed"
+                    priority={false}
+                  />
+                )}
+
                 {step.text}
               </Button>
-
-
             </StepButton>
           </Step>
         ))}
       </Stepper>
       <div>
         <React.Fragment>
-
-          {activeStep === 0 ?
-            (<Typography sx={{ mt: 2, mb: 1, py: 1 }}>
+          {activeStep === 0 ? (
+            <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
               Step {activeStep + 1} - texto 1
-              <Image
-                className='w-32'
-                src={boxopened}
-                alt="box-closed"
-                priority={false}
-              />
             </Typography>
-
-
-            )
-
-            : (activeStep === 1) ? (<Typography sx={{ mt: 2, mb: 1, py: 1 }}>
+          ) : activeStep === 1 ? (
+            <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
               Step {activeStep + 1} - texto 2
-            </Typography>) : (
-
-              <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
-                Step {activeStep + 1} - texto 3
-              </Typography>
-            )}
-
+            </Typography>
+          ) : (
+            <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
+              Step {activeStep + 1} - texto 3
+            </Typography>
+          )}
         </React.Fragment>
       </div>
     </Box>
