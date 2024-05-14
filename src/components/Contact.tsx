752e31df1b954d08ccdb1c212133521641b8f57a
jess <jessiewq1@gmail.com>
2024-05-13 21:38:28 -0300

    fix: modal

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -37,8 +37,7 @@ export default function Contact() {
       setOpenModal(true);
       setModalTitle(`Seu e-mail foi enviado com sucesso!`);
       setModalText(
-        `${
-          e.target.name.value.split(" ")[0]
+        `${e.target.name.value.split(" ")[0]
         }, entraremos em contato assim que possível!`
       );
 
@@ -120,38 +119,49 @@ export default function Contact() {
         </div>
       </div>
 
-      <Dialog open={openModal} handler={handleOpen}>
-        <DialogHeader>
-          <Typography variant="h5" color="blue-gray">
-            {modalTitle}
-          </Typography>
-        </DialogHeader>
-        <DialogBody divider className="grid place-items-center gap-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="h-16 w-16 text-red-500"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5.25 9a6.75 6.75 0 0113.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 01-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 11-7.48 0 24.585 24.585 0 01-4.831-1.244.75.75 0 01-.298-1.205A8.217 8.217 0 005.25 9.75V9zm4.502 8.9a2.25 2.25 0 104.496 0 25.057 25.057 0 01-4.496 0z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <Typography color="blue-gray" variant="h4">
-            {modalText}
-          </Typography>
-          <Typography className="text-center font-normal">
-            Atenciosamente, equipe Torabox!
-          </Typography>
-        </DialogBody>
-        <DialogFooter className="space-x-2">
-          <Button variant="text" color="blue-gray" onClick={handleOpen}>
-            close
-          </Button>
-        </DialogFooter>
-      </Dialog>
-    </section>
+      {/* --------------------------MODAL */}
+
+      <div className='flex min-w-full justify-center items-center bg-blue-500 '>
+
+        <Dialog open={openModal} handler={handleOpen}>
+          <DialogHeader>
+            <Typography variant="h5" color="blue-gray">
+              {modalTitle}
+            </Typography>
+          </DialogHeader>
+
+          <div className='flex min-w-full justify-center items-center bg-pink-500'>
+            <DialogBody divider className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="h-16 w-16 text-red-500"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.25 9a6.75 6.75 0 0113.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 01-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 11-7.48 0 24.585 24.585 0 01-4.831-1.244.75.75 0 01-.298-1.205A8.217 8.217 0 005.25 9.75V9zm4.502 8.9a2.25 2.25 0 104.496 0 25.057 25.057 0 01-4.496 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <Typography color="blue-gray" variant="h4">
+                {modalText}
+              </Typography>
+              <Typography className="text-center font-normal">
+                Atenciosamente, equipe Torabox!
+              </Typography>
+            </DialogBody>
+          </div>
+
+          <DialogFooter className="space-x-2">
+            <Button variant="text" color="blue-gray" onClick={handleOpen}>
+              close
+            </Button>
+          </DialogFooter>
+        </Dialog>
+
+      </div>
+
+    </section >
   );
 }
